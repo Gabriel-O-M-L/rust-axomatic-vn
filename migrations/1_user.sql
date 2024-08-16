d9ff5ba1ae390765a7fb3d8c38049c7858c7e972
Gabriel Oliveira Moura Lima <qualifiedwind@gmail.com>
2024-08-16 09:42:28 -0400

    modifications to model

--- migrations/1_user.sql
+++ migrations/1_user.sql
@@ -1,7 +1,1 @@
-create table if not exists "user"
-(
-    user_id       uuid primary key default gen_random_uuid(),
-    username      text unique not null,
-    email         text unique not null,
-    password_hash text        not null
-);
+ad
